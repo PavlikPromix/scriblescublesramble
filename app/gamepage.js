0fb3b96169ee0ab2c4da937e2057670e22c81324
FUCKNOPE <rimskiy_2017@mail.ru>
2024-03-17 18:45:06 +0300

    fff

--- app/gamepage.js
+++ app/gamepage.js
@@ -1,13 +1,14 @@
+import Image from "next/image";
+import "./page.css"
 export default function Home() {
 	return (
 		<>
 			<div className="maincontainer">
 			</div>
 			<div>
-				<img className="img1" src=""></img>
-				<img className="img2" src=""></img>
+				<Image className="img1" width={80} height={100} src="/rusi.svg" alt="" />
+				<Image className="img2" width={80} height={100} src="/rusi.svg" alt="" />
 			</div>
-
 		</>
 	);
 }
