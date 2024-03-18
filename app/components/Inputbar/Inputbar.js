86bf640ab2b1d22041995a976dffbad46b880403
PavlikPromix <beriya2004@mail.ru>
2024-03-18 22:48:49 +0300

    Made fully functional Input component

--- app/components/Inputbar/Inputbar.js
+++ app/components/Inputbar/Inputbar.js
@@ -1,37 +1,58 @@
 "use client";
+import { useState, useRef, useEffect } from "react";
 import styles from "./Inputbar.module.scss";
-import { useState } from "react";
 
-function Inputbar({ children, length }) {
-	const [letters, setletters] = useState([
-		<input className={styles.letter} key={0} maxLength={1} size={1} />,
-		<input className={styles.letter} key={1} maxLength={1} size={1} />,
-		<input className={styles.letter} key={2} maxLength={1} size={1} />,
-		<input className={styles.letter} key={3} maxLength={1} size={1} />,
-		<input className={styles.letter} key={4} maxLength={1} size={1} />,
-	]);
-	if (length > 5) {
-		for (let i = 5; i < length; i++) {
-			setletters([
-				...letters,
-				<input
-					className={styles.letter}
-					key={i}
-					maxLength={1}
-					size={1}
-				/>,
-			]);
-		}
-	}
-	return (
-		<>
-			<div className={styles.letterContainer}>
-				{letters.map((letter, index) => {
-                    return letter; 
-				})}
-			</div>
-		</>
-	);
+function Inputbar({ length = 5 }) {
+	const [inputValues, setInputValues] = useState(Array(length).fill(""));
+	const inputRefs = useRef([]);
+
+	useEffect(() => {
+		setInputValues(Array(length).fill(""));
+		inputRefs.current = inputRefs.current.slice(0, length);
+	}, [length]);
+
+	const handleInput = (index) => {
+		return (e) => {
+			const uppercaseValue = e.target.value.toUpperCase();
+			const newValues = [...inputValues];
+			newValues[index] = uppercaseValue;
+			setInputValues(newValues);
+
+			if (index < length - 1 && uppercaseValue) {
+				inputRefs.current[index + 1].focus();
+			}
+		};
+	};
+
+	const handleKeyDown = (index) => {
+		return (e) => {
+			if (e.key === "Backspace" && !inputValues[index]) {
+				const newValues = [...inputValues];
+				newValues[index] = "";
+				setInputValues(newValues);
+
+				if (index > 0) {
+					inputRefs.current[index - 1].focus();
+				}
+			}
+		};
+	};
+
+	const inputs = Array.from({ length }, (_, index) => (
+		<input
+			ref={(el) => (inputRefs.current[index] = el)}
+			className={styles.letter}
+			key={index}
+			maxLength={1}
+			size={1}
+			value={inputValues[index]}
+			onInput={handleInput(index)}
+			onKeyDown={handleKeyDown(index)}
+			style={{ textTransform: "uppercase" }}
+		/>
+	));
+
+	return <div className={styles.letterContainer}>{inputs}</div>;
 }
 
 export default Inputbar;
