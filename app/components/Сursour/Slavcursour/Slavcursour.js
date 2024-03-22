e9b021e1ee0adf8e1c0a6bc9c3970845fb2e8640
FUCKNOPE <rimskiy_2017@mail.ru>
2024-03-23 00:27:10 +0300

    ffff

--- app/components/Сursour/Slavcursour/Slavcursour.js
+++ app/components/Сursour/Slavcursour/Slavcursour.js
@@ -2,45 +2,30 @@
 import React, { useState, useEffect } from "react";
 import './style.css'
 
-function SlavCursor() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+const CustomCursor = () => {
+  const [position, setPosition] = useState({x: 0, y:0});
+  const [prevPosition, setPrevPosition]=useState({x: 0, y:0})
   
-  const [isPointer, setIsPointer] = useState(false);
-
-  const handleMouseMove = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-
-    const target = e.target;
-
-    setIsPointer(
-      window.getComputedStyle(target).getPropertyValue("cursor") === "pointer"
-    );
-  };
-
-  useEffect(() => {
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
+  useEffect(()=> {
+    const updatePosition = (e) => {
+      setPrevPosition(position);
+      setPosition ({ x: e.clientX , y: e.clientY});
     };
-  }, []);
-
-  
-  const flareSize = isPointer ? 0 : 30;
-
-  const cursorStyle = isPointer ? { left: "-100px", top: "-100px" } : {};
-
-  return (
-    <div
-      className={`flare ${isPointer ? "pointer" : ""}`}
-      style={{
-        ...cursorStyle,
-        left: `${position.x}px`,
-        top: `${position.y}px`,
-        width: `${flareSize}px`,
-        height: `${flareSize}px`,
-      }}
-    ></div>
+    window.addEventListener('mousemove',updatePosition);
+    return () => window.removeEventListener('mousemove', updatePosition);
+  },[position]);
+  const direction ={
+      isMovingLeft: position.x < prev.Position.x,
+      isMovingUp: position.y < prevPosition.y,
+  }
+return (
+  <div 
+    className="cursor-trail"
+    style={{
+      left: '${position.x}px',
+      top: '${position.y}px',
+      transform: 'translate(${direction.isMovingLeft ? '+50%':'-50%'}, ${direction.isMovingUp ? '+50%':'-50%'})', 
+    }}></div>
   );
-}
-
-export default SlavCursor;
+};
+export default CustomCursor;
--- app/slavshit/page.js
+++ app/slavshit/page.js
@@ -1,14 +1,15 @@
 import Buttonchik from "../components/Buttonchik/Buttonchik";
 import Gamebar from "../components/Gamebar/Gamebar";
 import Keyboard from "../components/Keyboard/Keyboard";
-import SlavCursor from "../components/Сursour/Slavcursour/Slavcursour";
+import CustomCursor from "../components/Сursour/Slavcursour/Slavcursour";
 export default function RUSI() {
 	return (
 		<>
 			<div className="maincontainer">
-				<SlavCursor/>
+				<CustomCursor/>
 				<Gamebar />
 				<Keyboard lang={"ru"}/>
+				
 			</div>
 		</>
 	);
