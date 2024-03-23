7b8bd889a0c0dd1f46d12a8285540e12467fcd81
PavlikPromix <beriya2004@mail.ru>
2024-03-23 04:40:15 +0300

    Slight code-style change

--- app/components/Buttonchik/Buttonchik.js
+++ app/components/Buttonchik/Buttonchik.js
@@ -7,8 +7,8 @@ import { useRouter } from 'next/navigation';
 export default function Buttonchik({children, onClick, redirect}) {
     const router = useRouter()
     return (
-        redirect ? 
-        <button className={styles.button} onClick={() => router.push(redirect)}>{children}</button> :
-        <button className={styles.button} onClick={onClick}>{children}</button>
+        redirect 
+        ? <button className={styles.button} onClick={() => router.push(redirect)}>{children}</button> 
+        : <button className={styles.button} onClick={onClick}>{children}</button>
     )
 }
