0b6a2e1b5ac63a27b4010dd51db600c0e49cfebe
PavlikPromix <beriya2004@mail.ru>
2024-03-31 14:47:17 +0300

    Added fontFamily prop to the Buttonchik

--- app/components/Buttonchik/Buttonchik.js
+++ app/components/Buttonchik/Buttonchik.js
@@ -10,21 +10,19 @@ export default function Buttonchik({
 	width = "fit-content",
 	height = "fit-content",
 	fontSize = "1em",
+	fontFamily = "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
 }) {
 	const router = useRouter();
-	return redirect ? (
+	return (
 		<button
 			className={styles.button}
-			style={{ width: width, height: height, fontSize: fontSize }}
-			onClick={() => router.push(redirect)}
-		>
-			{children}
-		</button>
-	) : (
-		<button
-			className={styles.button}
-			style={{ width: width, height: height, fontSize: fontSize }}
-			onClick={onClick}
+			style={{
+				width,
+				height,
+				fontSize,
+				fontFamily,
+			}}
+			onClick={redirect ? () => router.push(redirect) : onClick}
 		>
 			{children}
 		</button>
